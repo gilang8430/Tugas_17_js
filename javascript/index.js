6db8c03448e1c3cff2708811346caf7776c237d4
gilang_gans <gilangsos99@gmail.com>
2022-05-16 14:05:35 +0700

    Tugas 17 js
    Tugas ini untuk niomic

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+function ubahLowerUpper(){
+  var lower = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  console.log(lower.toLowerCase());
+  var upper = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  console.log(upper.toUpperCase());
+  var lower2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS";
+  console.log(lower2.toLowerCase());
+  var upper2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS";
+  console.log(upper2.toUpperCase());
+}
+
+ubahLowerUpper();
---
+++ javascript/index1.js
@@ -0,0 +1,6 @@
+function ASCII(){
+  var kode = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  console.log(kode.charCodeAt(0),"-",kode.charCodeAt(1),"-",kode.charCodeAt(2),"-",kode.charCodeAt(3),"-",kode.charCodeAt(4),"-",kode.charCodeAt(32))
+}
+
+ASCII();
